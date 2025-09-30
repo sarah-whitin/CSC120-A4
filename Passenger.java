bc666d9b856ea7d5b3e408e77c79e6588aaac80b
S. Whitin <se.whitin@gmail.com>
2025-09-30 19:16:29 -0400

    passenger

--- Passenger.java
+++ Passenger.java
@@ -5,4 +5,17 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+
+    private void boardCar(Car c){
+        boolean seatAvailable = c.addPassanger(this);
+        if (seatAvailable = true){
+            System.out.println(name+" boarded car");
+        }else{
+            System.out.println("No seats available");
+        }
+    }
+
+    private void getOffCar(Car c){
+        boolean passengers = c.removePassanger(this);
+    }
 }
